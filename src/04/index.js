439b820099573a396ea791aea011ed1d9e4ad9c2
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2022-12-17 22:51:52 -0300

    Day 04, Part 02

--- src/04/index.js
+++ src/04/index.js
@@ -1,13 +1,23 @@
 const { readFileLineByLine } = require("../utils/read-file");
 const path = require("node:path");
 
-function isOverlapped(sections) {
+function isFullyOverlapping(sections) {
   const pair1 = [sections[0], sections[1]];
   const pair2 = [sections[2], sections[3]];
 
   return (
     (pair1[0] <= pair2[0] && pair1[1] >= pair2[1]) ||
     (pair2[0] <= pair1[0] && pair2[1] >= pair1[1])
+  );
+}
+
+function isPartlyOverlapping(sections) {
+  const pair1 = [sections[0], sections[1]];
+  const pair2 = [sections[2], sections[3]];
+
+  return (
+    (pair1[1] >= pair2[0] && pair1[0] <= pair2[1]) ||
+    (pair2[1] >= pair1[0] && pair2[0] <= pair1[1])
   );
 }
 
@@ -20,7 +30,7 @@ async function resolvePart1() {
       pair.split("-").map((section) => parseInt(section))
     );
 
-    if (isOverlapped(sections)) {
+    if (isFullyOverlapping(sections)) {
       overlappedPairs += 1;
     }
   });
@@ -28,6 +38,24 @@ async function resolvePart1() {
   console.log("Day 04, Part 01:", overlappedPairs);
 }
 
+async function resolvePart2() {
+  let overlappedPairs = 0;
+
+  await readFileLineByLine(path.join(__dirname, "input.txt"), (line) => {
+    const pairs = line.split(",");
+    const sections = pairs.flatMap((pair) =>
+      pair.split("-").map((section) => parseInt(section))
+    );
+
+    if (isPartlyOverlapping(sections)) {
+      overlappedPairs += 1;
+    }
+  });
+
+  console.log("Day 04, Part 02:", overlappedPairs);
+}
+
 (async () => {
   await resolvePart1();
+  await resolvePart2();
 })();
