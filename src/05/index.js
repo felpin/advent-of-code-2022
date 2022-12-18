db95a0165dcb392a12dc46b13202a52de6719a65
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2022-12-17 23:16:59 -0300

    Day 05, Part 01

---
+++ src/05/index.js
@@ -0,0 +1,36 @@
+const { readFileLineByLine } = require("../utils/read-file");
+const path = require("node:path");
+
+async function resolvePart1(stacks) {
+  await readFileLineByLine(path.join(__dirname, "input.txt"), (line) => {
+    const order = line.split(" ");
+    const cratesToMove = parseInt(order[1]);
+
+    const stackToMoveFrom = parseInt(order[3]) - 1;
+    const stackToMoveTo = parseInt(order[5]) - 1;
+
+    for (let i = 0; i < cratesToMove; i++) {
+      stacks[stackToMoveTo].push(stacks[stackToMoveFrom].pop());
+    }
+  });
+
+  const topCrates = stacks.map((stack) => stack[stack.length - 1]).join("");
+
+  console.log("Day 05, Part 01:", topCrates);
+}
+
+(async () => {
+  const stacks = [
+    ["Z", "T", "F", "R", "W", "J", "G"],
+    ["G", "W", "M"],
+    ["J", "N", "H", "G"],
+    ["J", "R", "C", "N", "W"],
+    ["W", "F", "S", "B", "G", "Q", "V", "M"],
+    ["S", "R", "T", "D", "V", "W", "C"],
+    ["H", "B", "N", "C", "D", "Z", "G", "V"],
+    ["S", "J", "N", "M", "G", "C"],
+    ["G", "P", "N", "W", "C", "J", "D", "L"],
+  ];
+
+  await resolvePart1(stacks);
+})();
