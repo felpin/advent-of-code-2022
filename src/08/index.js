8d8a802b0eb1a194bafcc260ffdb2ea8a69657dc
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2022-12-23 23:56:20 -0300

    Day 08, Part 02

--- src/08/index.js
+++ src/08/index.js
@@ -1,6 +1,26 @@
 const { readFileLineByLine } = require("../utils/read-file");
 const path = require("node:path");
 
+function getScenicScore(height, sides) {
+  const scores = [];
+
+  for (const side of sides) {
+    let visibleTrees = 0;
+
+    for (const tree of side) {
+      visibleTrees++;
+
+      if (tree >= height) {
+        break;
+      }
+    }
+
+    scores.push(visibleTrees);
+  }
+
+  return scores.reduce((scenicScore, score) => scenicScore * score);
+}
+
 function getVisibleTrees(trees) {
   let lastTreeHeight = trees[0];
   let visibleTreesIndexes = [0];
@@ -20,13 +40,8 @@ function getVisibleTrees(trees) {
   return visibleTreesIndexes;
 }
 
-async function resolvePart1() {
-  const treeGrid = [];
+function resolvePart1(treeGrid) {
   const visibleTreesIndexes = [];
-
-  await readFileLineByLine(path.join(__dirname, "input.txt"), (line) => {
-    treeGrid.push([...line].map((value) => parseInt(value, 10)));
-  });
 
   for (let rowIndex = 0; rowIndex < treeGrid.length; rowIndex++) {
     const row = treeGrid[rowIndex];
@@ -63,6 +78,44 @@ async function resolvePart1() {
   console.log("Day 08, Part 01:", set.size);
 }
 
+function resolvePart2(grid) {
+  let highestScenicScore = 0;
+
+  for (let rowIndex = 1; rowIndex < grid.length - 1; rowIndex++) {
+    const row = grid[rowIndex];
+
+    for (let columnIndex = 1; columnIndex < row.length - 1; columnIndex++) {
+      const top = grid
+        .slice(0, rowIndex)
+        .map((row) => row[columnIndex])
+        .reverse();
+
+      const bottom = grid.slice(rowIndex + 1).map((row) => row[columnIndex]);
+      const left = row.slice(0, columnIndex).reverse();
+      const right = row.slice(columnIndex + 1);
+
+      const score = getScenicScore(grid[rowIndex][columnIndex], [
+        top,
+        bottom,
+        left,
+        right,
+      ]);
+
+      if (score > highestScenicScore) {
+        highestScenicScore = score;
+      }
+    }
+  }
+
+  console.log("Day 08, Part 02:", highestScenicScore);
+}
+
 (async () => {
-  await resolvePart1();
+  const treeGrid = [];
+  await readFileLineByLine(path.join(__dirname, "input.txt"), (line) => {
+    treeGrid.push([...line].map((value) => parseInt(value, 10)));
+  });
+
+  resolvePart1(treeGrid);
+  resolvePart2(treeGrid);
 })();
